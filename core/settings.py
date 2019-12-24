57354ffd8ead418c0c5b20fe90885d6826fa7e45
Piterskikh <piterskikhsa@gmail.com>
2019-12-24 10:04:05 +0300

    fix remove allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['192.168.31.129', 'localhost']
+ALLOWED_HOSTS = []
 
 # Application definition
 
